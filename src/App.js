22426d8db07271a09e6fc3119030136ce8e62f9d
Ajulrag.b <ajulrag353@gmail.com>
2023-07-20 12:54:54 +0530

    feat: About page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserLayout from './components/layout/UserLayout';
 import Home from './components/home/hero/Home';
+import About from './components/about/About';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<UserLayout />} children={[
-            <Route path='/' element={<Home />}/>
+            <Route path='/' element={<Home />}/>,
+            <Route path='/about' element={<About />}/>
           ]} />
         </Routes>
       </Router>
---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Back from "../common/back/Back"
+const About = () => {
+    return(
+        <>
+        <Back title='About Us' />
+        </>
+    )
+}
+
+export default About
