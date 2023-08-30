fc02d4f3e43c9759d710d4006c384c6df0693d89
Ajulrag.b <ajulrag353@gmail.com>
2023-08-30 19:56:38 +0530

    feat:user status checking

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,41 +2,48 @@ import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { setUserDetails } from "../redux/UserSlice";
+import { useDispatch } from "react-redux";
+
 
 
 const PrivateRoutes = ({ children }) => {
     const navigate = useNavigate()
     const location = useLocation()
+    const dispatch = useDispatch()
 
     useEffect(() => {
+        const token = localStorage.getItem('userToken');
 
-        const token = localStorage.getItem('userToken')
         if (!token) {
-            navigate('/auth')
+            navigate('/auth');
         } else {
+            console.log(token, "token");
             const fetchProfile = async () => {
                 try {
                     const response = await axios.get('/getprofile', {
                         headers: {
-                            'autherization': `Bearer ${localStorage.getItem('userToken')}`
-                        }
-                    },
-                        {
-                            credentials: true
-                        }
-                    )
-                    console.log("hello", response.data.user);
-                    dispatchEvent(setUserDetails(response.data.user))
+                            'autherization': `${token}`
+                        },
+                        // withCredentials: true
+                    });
+                    console.log(response , "priba");
+                    const userDetails = response.data.user;
+                    dispatch(setUserDetails(userDetails));
+
+                    if (userDetails.status === 'Inactive') {
+                        localStorage.removeItem('userToken');
+                        navigate('/auth');
+                    }
 
                 } catch (err) {
                     console.log(err);
                 }
             }
 
-            fetchProfile()
+            fetchProfile();
         }
+    }, [location, navigate, dispatch]);
 
-    }, [location, navigate])
 
     return children;
 }
